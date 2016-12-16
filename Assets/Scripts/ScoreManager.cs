6dd5742631aa291098e2dad848d5145df00e6535
Rodrigo Medina <rckdrigomed@gmail.com>
2016-12-16 11:06:37 +0700

    Adding post/read highscores, color feedback when correct/incorrect, updating prefabs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ScoreManager : MonoBehaviour
@@ -7,18 +8,26 @@ public class ScoreManager : MonoBehaviour
 
 	[SerializeField] private int minScoreToWin = 3;
 	[SerializeField] private int minFailsToLose = 2;
+	[SerializeField] private AudioClip correctSFX, wrongSFX;
+
+	[SerializeField] private Image button;
+	[SerializeField] private Text winText, loseText;
 
 	private new AudioSource audio;
-	public AudioClip correctSFX, wrongSFX;
 
 	private int currentScore = 0;
+	private int currentHighscore = 0;
 
 	public int CurrentScore { 
 		get { return currentScore; } 
 		set { 
 			currentScore = value; 
 			audio.PlayOneShot (correctSFX);
+			StopAllCoroutines ();
+			StartCoroutine (ChangeColor (true));
 			if (currentScore == minScoreToWin) {
+				winText.text = "Highscore : " + currentHighscore + "\nScore : " + currentScore;
+				StartCoroutine (ScorePost ());
 				currentScore = fails = 0;
 				QuestionManager.Instance.CleanUsedQuestions ();
 				ScreenController.Instance.WinGame ();
@@ -33,7 +42,11 @@ public class ScoreManager : MonoBehaviour
 		set { 
 			fails = value; 
 			audio.PlayOneShot (wrongSFX);
+			StopAllCoroutines ();
+			StartCoroutine (ChangeColor (false));
 			if (fails == minFailsToLose) {
+				loseText.text = "Highscore : " + currentHighscore + "\nScore : " + currentScore;
+				StartCoroutine (ScorePost ());
 				currentScore = fails = 0;
 				QuestionManager.Instance.CleanUsedQuestions ();
 				ScreenController.Instance.LoseGame ();
@@ -41,10 +54,61 @@ public class ScoreManager : MonoBehaviour
 		} 
 	}
 
+	IEnumerator ChangeColor (bool correct)
+	{
+		Color c = correct ? Color.green : Color.red;
+
+		while (button.color != c) {
+			button.color = Color.Lerp (c, button.color, Time.deltaTime);
+			yield return null;
+		}
+
+		c = !correct ? Color.green : Color.red;
+		while (button.color != c) {
+			button.color = Color.Lerp (Color.white, button.color, Time.deltaTime);
+			yield return null;
+		}
+	}
+
 	void Start ()
 	{
 		Instance = this;
 		audio = GetComponent<AudioSource> ();
+
+		StartCoroutine (ScoreRead ());
 	}
 
+	IEnumerator ScorePost ()
+	{
+		if (currentScore > currentHighscore) {
+			currentHighscore = currentScore;
+			print ("posting score");
+			WWWForm form = new WWWForm ();
+			form.AddField ("highscore", currentScore);
+
+			WWW www = new WWW ("https://digitalairtest.000webhostapp.com/postHighScore.php", form);
+			yield return www;
+			if (!string.IsNullOrEmpty (www.error)) {
+				print (www.error);
+			} else {
+				print ("Finished Uploading High Score");
+			}
+		} else
+			yield break;
+	}
+
+	IEnumerator ScoreRead ()
+	{
+
+		WWW www = new WWW ("https://digitalairtest.000webhostapp.com/highScores.txt");
+		yield return www;
+		if (!string.IsNullOrEmpty (www.error))
+			print (www.error);
+		else
+			print ("Finished Reading High Score");
+
+		print ("www " + www.text);
+		currentHighscore = int.Parse (www.text);
+
+	}
 }
--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -11,15 +11,6 @@ public class TimerManager : MonoBehaviour
 
 	private Text timerText;
 
-	void Awake ()
-	{
-	}
-
-	//	public void OnEnable ()
-	//	{
-	//		StartTimer ();
-	//	}
-
 	public void StartTimer ()
 	{
 		timerText = GetComponent<Text> ();
@@ -34,8 +25,8 @@ public class TimerManager : MonoBehaviour
 	IEnumerator RunTimer ()
 	{
 		for (int i = maxWaitingTime; i >= 0; i--) {
-			timerText.color = Color.Lerp (Color.red, Color.white, (float)i / maxWaitingTime);
-			timerText.text = i + " s";
+			timerText.color = Color.Lerp (Color.red, Color.black, (float)i / maxWaitingTime);
+			timerText.text = i.ToString ();
 			yield return new WaitForSecondsRealtime (1f);
 		}
 		if (qm.group.AnyTogglesOn ())
